75515ef1574b9b0bbbba2e6d616443854faae7bc
Doxie <terrance.doxie@bestwestern.com>
2020-10-02 12:28:15 -0700

    making updates to modules

--- stage/data-stores/mysql/variables.tf
+++ stage/data-stores/mysql/variables.tf
@@ -1,7 +1,17 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "db_password" {
   description = "The password for the database"
   type        = string
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "db_name" {
   description = "The name to use for the database"
