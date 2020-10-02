9150b284e2de52207babd03ec1e4f2249c651116
Doxie <terrance.doxie@bestwestern.com>
2020-10-02 13:45:14 -0700

    tesing git fetch

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -143,11 +143,11 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "db" {
-    backend = "s3"
-    config = {
-        bucket = var.db_remote_state_bucket
-        key = var.db_remote_state_key
-        region = "us-east-2"
-    }
-}
+#data "terraform_remote_state" "db" {
+#    backend = "s3"
+#    config = {
+#        bucket = var.db_remote_state_bucket
+#        key = var.db_remote_state_key
+#        region = "us-east-2"
+#    }
+#}
