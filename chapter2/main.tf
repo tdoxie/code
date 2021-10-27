a9041582776cbe6cdcc0a74e679965fe3d9e5a61
Terrance Doxie <tdoxie@gmail.com>
2021-10-27 00:32:10 -0700

    chapter 1 practice

---
+++ chapter2/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami          = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance"{
+  name = "terraform-example-instance"
+   ingress {
+       from_port = var.server_port
+       to_port   = var.server_port
+       protocol = "tcp"
+       cidr_blocks =  ["0.0.0.0/0"]
+   }
+}
+
+output "Public_IP" {
+  value = aws_instance.example.public_ip
+}
